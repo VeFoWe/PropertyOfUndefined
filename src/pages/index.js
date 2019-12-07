54deb446253bf5df2a3ad7e81691bd6e2eed73f2
Crispin Mårtens <ChrisBean@Illium.local>
2019-12-07 18:04:45 +0100

    Upload for Troubleshooting

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,33 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import TransitionLink from 'gatsby-plugin-transition-link'
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>Test</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+
+
+    <TransitionLink
+  to="/page-2"
+  exit={{
+    trigger: ({ exit, node }) => this.interestingExitAnimation(exit, node),
+    length: 1
+  }}
+  entry={{
+    delay: 0.6
+  }}
+>
+  Go to page 2
+</TransitionLink>
+
   </Layout>
 )
 
